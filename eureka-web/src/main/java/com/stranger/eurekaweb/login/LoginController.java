e860c00d00314e8402f6bb1fe835835b4d8ff801
stranger <849951020@qq.com>
2020-08-16 21:38:14 +0800

    add Login html controller

--- eureka-web/src/main/java/com/stranger/eurekaweb/login/LoginController.java
+++ eureka-web/src/main/java/com/stranger/eurekaweb/login/LoginController.java
@@ -1,14 +1,8 @@
 package com.stranger.eurekaweb.login;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
-
-    @GetMapping("/login")
-    public String login(){
-        return "system/login";
-    }
 
 }
